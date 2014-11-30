adf7e50babf046ae20d68f19c283cbd4fd10f8d5
Alejandro Riera <ariera@gmail.com>
2014-11-30 15:28:05 +0100

    some documentation for offline reading

---
+++ docs/Introduction to Thor_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"http:\/\/www.sitepoint.com\/unraveling-string-key-performance-ruby-2-2\/","comments":4},{"id":"http:\/\/www.sitepoint.com\/mini-chat-rails-server-sent-events\/","comments":16}]});
+}
